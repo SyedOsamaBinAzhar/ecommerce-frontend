9eb263e89ec7c609da91d6ee686f8b13a6bffafb
SyedOsamaBinAzhar <osamaazhar.edu@gmail.com>
2022-03-09 21:59:10 +0500

    Signup and Login Page Ready.

---
+++ src/pages/Login.jsx
@@ -0,0 +1,74 @@
+import styled from "styled-components"
+import loginImg from "../assets/home/loginImg.jpg";
+
+const Container = styled.div`
+  width : 100vw;
+  height : 100vh;
+  display : flex;
+  justify-content : center;
+  align-items : center;
+  background : linear-gradient(rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.5)), url(${loginImg});
+  background-size : cover;
+
+`
+
+const Wrapper = styled.div`
+  padding : 20px;
+  width : 25%;
+  background-color : white;
+
+`
+
+const Title = styled.form`
+  font-size : 24px;
+  font-weight : 300;
+
+`
+
+const Form = styled.h1`
+  display : flex;
+  flex-direction : column;
+`
+
+const Input = styled.input`
+  flex : 1;
+  min-width : 40%;
+  margin : 10px 0px;
+  padding : 10px;
+`
+
+const Button = styled.button`
+  width : 40%;
+  border : none;
+  padding : 15px 20px;
+  background-color : teal;
+  color : white;
+  cursor : pointer;
+  margin-bottom  10px;
+`
+const Link = styled.a`
+    margin : 15px 0px;
+    font-size : 12px;
+    text-decoration : underline;
+    cursor : pointer;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN.</Title>
+        <Form>
+          <Input placeholder = "username"/>
+          <Input placeholder = "password"/>
+          <Button>CREATE</Button>
+          <Link>Forgot Password?</Link>
+          <Link>Create A New Account.</Link>
+
+        </Form>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Login
