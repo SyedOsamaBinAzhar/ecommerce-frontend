13526fe4c1836570b586cdcd9ac7019d531a8288
SyedOsamaBinAzhar <osamaazhar.edu@gmail.com>
2022-03-09 13:26:14 +0500

    Categories Layout Ready.

--- src/data.js
+++ src/data.js
@@ -3,6 +3,12 @@ import coverTwo from "../src/assets/home/coverTwo.jpg";
 import coverThree from "../src/assets/home/coverThree.jpg";
 
 
+import jeansCateg from "../src/assets/home/jeansCateg.jpg";
+import summerCateg from "../src/assets/home/summerCateg.jpg";
+import jacketCateg from "../src/assets/home/jacketCateg.jpg";
+
+
+
 export const sliderItems = [
     {
         id : 1,
@@ -24,5 +30,23 @@ export const sliderItems = [
         title : "POPULAR SALE",
         desc : "Get Flat 30% Off On New Arrivals.",
         bg : "f5fafd"
+    }
+]
+
+export const categories = [
+    {
+        id : 1,
+        img : summerCateg,
+        title : "SHIRT STYLE"
+    },
+    {
+        id : 1,
+        img : jeansCateg,
+        title : "JEANS STYLE"
+    },
+    {
+        id : 1,
+        img : jacketCateg,
+        title : "JACKET STYLE"
     }
 ]
